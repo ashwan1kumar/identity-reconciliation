e4e388aca986cb0e330401efacb60d49a5707b4d
Ashwani <krashwani0908@gmail.com>
2024-06-30 21:09:29 +0530

    Some minor changes

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,7 +4,6 @@ require('dotenv').config()
 console.log(process.env.PORT)
 const sequelize = new Sequelize(process.env.DB_URI, {
   dialect: "postgres",
-  host: "postgresql://identity_xbn2_user:Jzpq227Vd2LkgWaNHzcUBhRKkNepLZGT@dpg-cq0jearv2p9s73cb84a0-a.oregon-postgres.render.com/identity_xbn2",
   port: 5432,
   password: process.env.DB_PASS,
   user: process.env.DB_USER,
